6c1c611fcd0cda750d867d52d2e6c261ffb8ac07
Rishi Thakur <rishithakur9131@gmail.com>
2025-03-03 15:41:48 +0530

    [Rishi] Add. Create a Rest Controller to demonstrate the various HTTP Methods

--- addressbook/src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
+++ addressbook/src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// UC-1 Project Setup
 @SpringBootApplication
 
 public class AddressbookApplication {
